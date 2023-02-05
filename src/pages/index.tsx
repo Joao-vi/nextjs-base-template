383c900a20c95cd1a56adc1d6f18a7cc80f9d231
joao-vi <joaovictor.go.ro@gmail.com>
2023-02-05 12:22:40 -0400

    chore: delete unused files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={inter.className}></main>
+      <main className={inter.className}>
+        <h1>Hello World</h1>
+      </main>
     </>
   );
 }
